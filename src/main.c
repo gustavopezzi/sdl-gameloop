6b4e520c69849fd4bb0f43ad1f66d52bf15807ce
Gustavo Pezzi <gpezzi@gpezzi.local>
2023-04-22 19:12:17 +0100

    Added a loop around SDL_PollEvent to handle multiple events.

--- src/main.c
+++ src/main.c
@@ -56,16 +56,17 @@ int initialize_window(void) {
 ///////////////////////////////////////////////////////////////////////////////
 void process_input(void) {
     SDL_Event event;
-    SDL_PollEvent(&event);
-    switch (event.type) {
-        case SDL_QUIT:
-            game_is_running = false;
-            break;
-        case SDL_KEYDOWN:
-            if (event.key.keysym.sym == SDLK_ESCAPE) {
+    while (SDL_PollEvent(&event)) {
+        switch (event.type) {
+            case SDL_QUIT:
                 game_is_running = false;
-            }
-            break;
+                break;
+            case SDL_KEYDOWN:
+                if (event.key.keysym.sym == SDLK_ESCAPE) {
+                    game_is_running = false;
+                }
+                break;
+        }
     }
 }
 
